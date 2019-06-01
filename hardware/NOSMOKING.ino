ca9108b14574556e55d7dc5a39799dc95e8d0b1e
Jung Chaejin <karma_777@naver.com>
2019-06-01 19:30:00 +0900

    Add files via upload

--- hardware/NOSMOKING.ino
+++ hardware/NOSMOKING.ino
@@ -1,31 +1,16 @@
-/* 
- Citizen Sensor
- http://citizensensor.cc
- MQ-7 Carbon Monoxide Sensor
- Power Cycle + Analog Read
- 
- for this example:
- - the "tog" pin of the breakout should be connected to digital pin 12.
- - the "out" pin of the breakout should be connected to analog pin 0.
- 
- >> When the sensor is receiving 5v of power, the LED Indicator (Pin 13) will
- >> be ON.  During this time data the data being output is NOT readable.
- >> When the sensor is receiving 1.4v or power, the LED Indicator will be
- >> OFF.  This when the data being output is USABLE. 
- 
- */
-
 #include "CS_MQ7.h"
 #include <SoftwareSerial.h>
 
-
+ 
 int Tx=4;
 int Rx=5;
+int sensorValue[1] = {0};
 
-SoftwareSerial BTSerial(Tx, Rx);CS_MQ7 MQ7(3, 13);  // 12 = digital Pin connected to "tog" from sensor board
-                     // 13 = digital Pin connected to LED Power Indicator
+SoftwareSerial BTSerial(Tx, Rx);
+CS_MQ7 MQ7(3, 13);  // 12 = digital Pin connected to "tog" from sensor board
 int CoSensorOutput = 0; //analog Pin connected to "out" from sensor board
 int CoData = 0;         //analog sensor data
+ 
 
 void setup(){
   BTSerial.begin(9600);
@@ -33,19 +18,32 @@ void setup(){
 }
 
 void loop(){
-  MQ7.CoPwrCycler();  
 
-    CoData = analogRead(CoSensorOutput);
-    //Serial.println(CoData);
-    //if(CoData>=100){
-    //    Serial.println(CoData);
-    //      }
-    if(BTSerial.available()){
-      Serial.write(BTSerial.read());
-      }
-    if(Serial.available()){
-      BTSerial.write(Serial.read());
-    }
-    
-  delay(300);      
+  MQ7.CoPwrCycler();  
+  sensorValue[0] = analogRead(CoSensorOutput);
+  if(0<sensorValue[0] && sensorValue[0] < 23){
+    BTSerial.write(Serial.read());
+    BTSerial.println("0");
+//    BTSerial.println(" ");
+    Serial.println("0");
+  }
+  else if(23 <= sensorValue[0] && sensorValue[0] <= 60){
+    BTSerial.write(Serial.read());
+    BTSerial.println("1");
+//    BTSerial.println(" ");
+    Serial.println("1");
+  }
+//  else if(sensorValue[0] > 75){
+else{
+    BTSerial.write(Serial.read());
+    BTSerial.println("2");
+//    BTSerial.println(" ");
+    Serial.println("2");
+  }
+  
+//  
+//  Serial.print(sensorValue[0]);
+//  Serial.println();
+  
+  delay(500);    
 }
